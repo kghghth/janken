859a063fcc73ef6d352c1334c8d741a7959c9c6a
kghghth <n8s6b4C9@iclud.com>
2020-08-11 09:12:27 +0900

    修正

--- app/src/main/java/com/e/my/MainActivity.kt
+++ app/src/main/java/com/e/my/MainActivity.kt
@@ -3,9 +3,12 @@ package com.e.my
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.prefs.Preferences
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +17,15 @@ class MainActivity : AppCompatActivity() {
         gu.setOnClickListener { onJankenButtonTapped(it)}
         choki.setOnClickListener { onJankenButtonTapped(it)}
         pa.setOnClickListener { onJankenButtonTapped(it)}
-    }
 
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        pref.edit  {
+            clear()
+        }
+
+
+
+    }
 
     fun onJankenButtonTapped(view: View?) {
         val intent = Intent(this, ResultActivity::class.java)
--- app/src/main/java/com/e/my/ResultActivity.kt
+++ app/src/main/java/com/e/my/ResultActivity.kt
@@ -1,17 +1,106 @@
+@file:Suppress("DEPRECATION")
+
 package com.e.my
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_result.*
 
+@Suppress("DEPRECATION")
 class ResultActivity : AppCompatActivity() {
 
     val gu = 0
     val choki = 1
     val pa = 2
+
+        private fun getHand(): Int {
+            var hand = (Math.random() * 3).toInt()
+            val pref = PreferenceManager.getDefaultSharedPreferences(this)
+            val gameCount = pref.getInt("GAME_COUNT", 0)
+            val winnigStreakCount = pref.getInt("WINNING_STREAK_COUNT", 0)
+            val lastMyHamd = pref.getInt("LAST_MY_HAND", 0)
+            val lastComHand = pref.getInt("lAST_COM_HAND", 0)
+            val beforelastComHand = pref.getInt("BEFORE_LAST_COM_HAND", 0)
+            val gameResult = pref.getInt("GAME_RESULT", -1)
+
+
+            if (gameCount == 1) {
+                if (gameResult == 2) {
+                    // 前回の勝負が1回目で、コンピュータが勝った場合、
+                    // コンピュータは次に出す手を変える
+                    while (lastComHand == hand) {
+                        hand = (Math.random() * 3).toInt()
+                    }
+                } else if (gameResult == 1) {
+                    // 前回の勝負が1回目で、コンピュータが負けた場合
+                    // 相手の出した手に勝つ手を出す
+                    hand = (lastMyHand - 1 + 3) % 3
+                }
+            } else if (winningStreakCount > 0) {
+                if (beforeLastComHand == lastComHand) {
+                    //同じ手で連勝した場合は手を変える
+                    while (lastComHand == hand) {
+                        hand = (Math.random() * 3).toInt()
+                    }
+                }
+            }
+            return hand
+        }
+
+
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+         private fun saveDate(myHand: Int, comHand: Int, gameResult: Int) {
+            val pref = PreferenceManager.getDefaultSharedPreferences(this)
+            val gameCount = pref.getInt("GAME_COUNT", 0)
+            val winningStreakCount = pref.getInt("WINNING_STREAk_COUNT", 0)
+            val lastComHand = pref.getInt("lAST_COM_HAND", 0)
+            val lastGameResult = pref.getInt("GAME_RESULT", -1)
+
+            val edtWinningStreakCount: Int =
+                when {
+                    lastGameResult == 2 && gameResult == 2 ->
+                        winningStreakCount + 1
+                    else ->
+                        0
+                }
+            val editor = pref.edit()
+            editor.putInt("GAME_COUNT",gameCount + 1)
+                .putInt("WINNING_STREAK_COUNT", edtWinningStreakCount)
+                .putInt("LAST_MY_HAND", myHand)
+                .putInt("LAST_COM_HAND", comHand)
+                .putInt("BEFFORE_LAST_COM_HAND", lastComHand)
+                .putInt("GAME_RESULT", gameResult)
+                .apply()
+        fun saveDate(myHand: Int, comHand: Int, gameResult: Int) {
+            val pref = PreferenceManager.getDefaultSharedPreferences(this)
+            val gameCount = pref.getInt("GAME_COUNT", 0)
+            val winningStreakCount = pref.getInt("WINNING_STREAk_COUNT", 0)
+            val lastComHand = pref.getInt("lAST_COM_HAND", 0)
+            val lastGameResult = pref.getInt("GAME_RESULT", -1)
+
+            val edtWinningStreakCount: Int =
+                when {
+                    lastGameResult == 2 && gameResult == 2 ->
+                        winningStreakCount + 1
+                    else ->
+                        0
+                }
+            val editor = pref.edit()
+            editor.putInt("GAME_COUNT",gameCount + 1)
+                .putInt("WINNING_STREAK_COUNT", edtWinningStreakCount)
+                .putInt("LAST_MY_HAND", myHand)
+                .putInt("LAST_COM_HAND", comHand)
+                .putInt("BEFFORE_LAST_COM_HAND", lastComHand)
+                .putInt("GAME_RESULT", gameResult)
+                .apply()
+        }
         setContentView(R.layout.activity_result)
+
+
         val id = intent.getIntExtra("MY_HAND",0)
 
         val myHand: Int
